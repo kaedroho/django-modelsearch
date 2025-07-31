4cbab3576517d6bd2e4e401a078d304fdf015240
Karl Hobley <karl@kaed.uk>
2025-07-31 17:01:23 +0100

    Add a base backend for common Elasticsearch/OpenSearch code

--- modelsearch/backends/opensearch1.py
+++ modelsearch/backends/opensearch1.py
@@ -1,39 +1,47 @@
-from modelsearch.backends.elasticsearch7 import (
-    Elasticsearch7AutocompleteQueryCompiler,
-    Elasticsearch7Index,
-    Elasticsearch7Mapping,
-    Elasticsearch7SearchBackend,
-    Elasticsearch7SearchQueryCompiler,
-    Elasticsearch7SearchResults,
+from opensearchpy import OpenSearch, NotFoundError
+from opensearchpy.helpers import bulk
+
+from modelsearch.backends.elasticsearchbase import (
+    ElasticsearchBaseAutocompleteQueryCompiler,
+    ElasticsearchBaseIndex,
+    ElasticsearchBaseMapping,
+    ElasticsearchBaseSearchBackend,
+    ElasticsearchBaseSearchQueryCompiler,
+    ElasticsearchBaseSearchResults,
 )
 
 
-class OpenSearch1Mapping(Elasticsearch7Mapping):
+class OpenSearch1Mapping(ElasticsearchBaseMapping):
     pass
 
 
-class OpenSearch1Index(Elasticsearch7Index):
+class OpenSearch1Index(ElasticsearchBaseIndex):
     pass
 
 
-class OpenSearch1SearchQueryCompiler(Elasticsearch7SearchQueryCompiler):
+class OpenSearch1SearchQueryCompiler(ElasticsearchBaseSearchQueryCompiler):
     mapping_class = OpenSearch1Mapping
 
 
-class OpenSearch1SearchResults(Elasticsearch7SearchResults):
+class OpenSearch1SearchResults(ElasticsearchBaseSearchResults):
     pass
 
 
-class OpenSearch1AutocompleteQueryCompiler(Elasticsearch7AutocompleteQueryCompiler):
+class OpenSearch1AutocompleteQueryCompiler(ElasticsearchBaseAutocompleteQueryCompiler):
     mapping_class = OpenSearch1Mapping
 
 
-class OpenSearch1SearchBackend(Elasticsearch7SearchBackend):
+class OpenSearch1SearchBackend(ElasticsearchBaseSearchBackend):
     mapping_class = OpenSearch1Mapping
     index_class = OpenSearch1Index
     query_compiler_class = OpenSearch1SearchQueryCompiler
     autocomplete_query_compiler_class = OpenSearch1AutocompleteQueryCompiler
     results_class = OpenSearch1SearchResults
+    NotFoundError = NotFoundError
+    client_class = OpenSearch
+
+    def bulk(self, *args, **kwargs):
+        return bulk(*args, **kwargs)
 
 
 SearchBackend = OpenSearch1SearchBackend
