c4cae39def589ca2b1cb8fac3f9eb73a0f1b8e18
Matthew Westcott <matthew@west.co.tt>
2025-09-22 21:16:35 +0100

    Don't call _process_match_none if check_only is true
    This makes it unused on the database backends, and can thus be removed.

--- modelsearch/backends/database/mysql/mysql.py
+++ modelsearch/backends/database/mysql/mysql.py
@@ -17,7 +17,6 @@ from django.db.models.fields import BooleanField, FloatField, TextField
 from django.db.models.functions.comparison import Cast
 from django.db.models.functions.text import Length
 from django.db.models.manager import Manager
-from django.db.models.query_utils import Q
 from django.utils.encoding import force_str
 from django.utils.functional import cached_property
 
@@ -522,9 +521,6 @@ class MySQLSearchQueryCompiler(BaseSearchQueryCompiler):
         results = self.queryset.filter(id__in=object_ids)
 
         return results
-
-    def _process_match_none(self):
-        return Q(pk__in=[])
 
 
 class MySQLAutocompleteQueryCompiler(MySQLSearchQueryCompiler):
